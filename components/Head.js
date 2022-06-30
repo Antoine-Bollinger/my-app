0e400baae1af076dc1a71b93d3c4e841bfcf30c4
Antoine Bollinger <antoine.bollinger@gmail.com>
2022-06-30 15:34:54 +0200

    🔍️ SEO

--- components/Head.js
+++ components/Head.js
@@ -19,14 +19,14 @@ export default function MainHead({ title = '' }) {
             <meta name="author" content={siteAuthor} />
             <link rel="icon" type="image/png" href="/favicon.png" />
             <link rel="icon" type="image/ico" href="/favicon.ico" />
-            <meta name="twitter:image:src" content="/preview.jpg" />
+            <meta name="twitter:image:src" content={`${siteUrl}/preview.jpg`} />
             <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:title" content={siteTitle} />
             <meta name="twitter:description" content={siteDescription} />
-            <meta property="og:image" content="/preview.jpg" />
+            <meta property="og:image" content={`${siteUrl}/preview.jpg`} />
             <meta property="og:image:alt" content={siteDescription} />
-            <meta property="og:image:width" content="1200" />
-            <meta property="og:image:height" content="600" />
+            <meta property="og:image:width" content="1434" />
+            <meta property="og:image:height" content="828" />
             <meta property="og:site_name" content={siteTitle} />
             <meta property="og:type" content="object" />
             <meta property="og:title" content={siteTitle} />
