e3543a035ce0baab0794e729ed7fd21e7ee38f54
Antoine Bollinger <antoine.bollinger@gmail.com>
2022-07-01 11:59:01 +0200

    💄 contact part

--- sections/Contact.js
+++ sections/Contact.js
@@ -7,7 +7,7 @@ import me from "../public/me.jpg";
 export default function Contact() {
     return (
         <Section id="contact" className="flex flex-col lg:flex-row justify-center items-center">
-            <div className="w-full h-full flex items-end justify-start">
+            <div className="w-full h-full flex items-end justify-start hidden lg:flex">
                 <div className="w-full lg:w-1/2 relative before:bg-orange-600 before:bottom-pal" id="identity">
                     <Image
                         priority
